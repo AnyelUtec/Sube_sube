cc70464397512ad8fb39e43489d05b07b3169135
AngelLinux <angelgabrielquino18@gmail.com>
2025-01-09 23:19:44 -0500

    se agrego la funcion "drive"

--- S6/holu.py
+++ S6/holu.py
@@ -6,3 +6,11 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
+
+
+
+drive = webdriver.chrome
+if drive :
+    print("asd")
+else :
+    print ("holuu")
